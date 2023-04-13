5723fb8036e5c2223e2687295f6d34e636fb4239
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-13 11:05:21 +0200

    final commit to IWA13

--- scripts.js
+++ scripts.js
@@ -4,25 +4,25 @@ let calculated = '1'
 
 // Only allowed to change below
 
-
 const logCalc = () => { 
     const isString = typeof calculated === 'string' 
     const calculatedAsNumber = isString ? parseInt(calculated) :(calculated)
     calculated = calculatedAsNumber + 1 
 }
-const = calcUser () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
-}
 
-const = checkUser () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
-}
-
+const calcUser = () => {
+    logCalc()
+    if (calculated > 2) user = 'John'
+    if (calculated > 2) state = 'requesting'
+    if (calculated > 3) state = 'idle'
+  }
+  
+  const checkUser = () => {
+      if (user && state === 'requesting') {
+          console.log(`User: ${user} (${calculated})`)
+      }
+  }
+  
 // Only allowed to change code above
 
 checkUser()
